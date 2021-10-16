c9461d3e6bc13ffbe90ea6a9d51deb3e8094adaf
Guilherme Feitosa <guifeitos@gmail.com>
2021-10-16 10:58:21 -0300

    Criação da Classe Pessoa e CadastroViewModel

---
+++ MVVM/Tela Cadastro + Alerts/Tela Cadastro + Alerts/Model/Pessoa.swift
@@ -0,0 +1,8 @@
+//
+//  Pessoa.swift
+//  Tela Cadastro + Alerts
+//
+//  Created by Gui Feitosa on 16/10/21.
+//
+
+import Foundation
---
+++ MVVM/Tela Cadastro + Alerts/Tela Cadastro + Alerts/ViewModel/CadastroViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CadastroViewModel.swift
+//  Tela Cadastro + Alerts
+//
+//  Created by Gui Feitosa on 16/10/21.
+//
+
+import Foundation
