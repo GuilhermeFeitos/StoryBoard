d11e78274454fd80535ba6e13fd88e221654573b
Guilherme Feitosa <guifeitos@gmail.com>
2021-10-16 16:12:38 -0300

    Criação das Telas e Componentes

---
+++ MVVM/Tela Cadastro + Alerts/Tela Cadastro_Alerts/Tela Cadastro_Alerts/SegundaTelaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SegundaTelaViewController.swift
+//  Tela Cadastro_Alerts
+//
+//  Created by Gui Feitosa on 16/10/21.
+//
+
+import UIKit
+
+class SegundaTelaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
