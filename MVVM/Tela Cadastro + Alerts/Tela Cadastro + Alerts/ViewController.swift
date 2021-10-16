6945a04a15eb10c3067b083800c97a9b15ee633a
Guilherme Feitosa <guifeitos@gmail.com>
2021-10-16 10:41:47 -0300

    Criação de Tela, Componentes e Organização dos Arquivo referente arquitetura MVVM

---
+++ MVVM/Tela Cadastro + Alerts/Tela Cadastro + Alerts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tela Cadastro + Alerts
+//
+//  Created by Gui Feitosa on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
